08a0ba4ef63f276a037e8e346af5b35e77786e16
Richard T. Anaya <160918134+Dokarii@users.noreply.github.com>
2025-04-02 10:03:05 -0500

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,47 @@
+/* rafce -> Crea un componente funcional  flecha */
+/* rfce -> Crea un componente funcional regular */
+import { useState } from "react";
+import "./Login.css";
+const Login = () => {
+  const [getUsuario, setUsuario] = useState("");
+  const [getPassword, setPassword] = useState("");
+  const [getHoraLogin, setHoraLogin] = useState(null);
+
+  function inicioSesion() {
+    if (getUsuario === "Admin" && getPassword === "admin") {
+      alert("Bienvenido");
+      let horaInicio = new Date();
+      console.log(horaInicio);
+      // setHoraLogin(new Date().toLocaleDateString());
+      // console.log(getHoraLogin);
+    } else alert("Usuario o contraseña incorrectos");
+    let horaIniciIncorrecto = new Date();
+    console.log(
+      horaIniciIncorrecto,
+      "Hora de inicio de sesión sospechoso o incorrecto"
+    );
+  }
+
+  return (
+    <form className="form">
+      Sign Up
+      <input
+        onChange={(e) => setUsuario(e.target.value)}
+        type="text"
+        className="input"
+        placeholder="Name"
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        className="input"
+        placeholder="Password"
+      />
+      <button type="button" onClick={inicioSesion}>
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Login;
